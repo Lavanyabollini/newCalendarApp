23f09512ba36741b9b999e224a5c7a1858f54af8
mobinius <narendra.v@mobinius.com>
2017-12-26 18:09:28 +0530

    commiting with added files

---
+++ NewCalendarApp/CustomCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomCell.swift
+//  NewCalendarApp
+//
+//  Created by mobinius on 18/12/17.
+//  Copyright © 2017 mobinius. All rights reserved.
+//
+
+import UIKit
+import JTAppleCalendar
+class CustomCell:JTAppleCell
+{
+    @IBOutlet weak var dateLabel:UILabel!
+    @IBOutlet weak var selectedView:UIView!
+
+}
